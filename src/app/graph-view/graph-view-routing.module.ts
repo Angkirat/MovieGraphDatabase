620f307c33021c069126d5a5ff0b9d9dfa095d03
Angkirat Sandhu <angkirat@gmail.com>
2021-11-13 14:48:14 +0000

    added bootstrap and Material theme

--- src/app/graph-view/graph-view-routing.module.ts
+++ src/app/graph-view/graph-view-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GraphViewComponent } from './graph-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:''
+    ,component: GraphViewComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
